44c9e1d51e462139a6d1484470c06215633be164
Sh1Sh0 <sh1sh0@abv.bg>
2022-09-24 12:51:03 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Choose your mode (Singleplayer, Multiplayer): ");
+
+            string mode = Console.ReadLine().ToLower();
+
+            if (mode == "singleplayer")
+            {
+                SinglePlayer();
+            }
+            else if (mode == "multiplayer")
+            {
+                Console.Write("Player 1 name:");
+
+                string playerOne = Console.ReadLine();
+
+                Console.Write("Player 2 name:");
+
+                string playerTwo = Console.ReadLine();
+
+                MultiPlayer(playerOne, playerTwo);
+            }
+        }
+
+        static void SinglePlayer()
+        {
+            int playerRandomNum;
+
+            int enemyRandonNum;
+
+            Random randomNums = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press ENTER to roll the dice.");
+
+                Console.ReadKey();
+
+                playerRandomNum = randomNums.Next(1, 7);
+                Console.WriteLine($"You rolled a {playerRandomNum}.");
+
+                enemyRandonNum = randomNums.Next(1, 7);
+                Console.WriteLine($"Enemy rolled a {enemyRandonNum}.");
+
+                if (playerRandomNum > enemyRandonNum)
+                {
+                    Console.WriteLine("You won!");
+                }
+                else if (playerRandomNum < enemyRandonNum)
+                {
+                    Console.WriteLine("Enemy won!");
+                }
+                else if (playerRandomNum == enemyRandonNum)
+                {
+                    Console.WriteLine("Tie!");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static void MultiPlayer(string playerOne, string playerTwo)
+        {
+            
+            int playerOneRandomNum;
+
+            int playerTwoRandomNum;
+
+            Random randomNums = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press ENTER to roll the dice.");
+
+                Console.ReadKey();
+
+                playerOneRandomNum = randomNums.Next(1, 7);
+                Console.WriteLine($"{playerOne} rolled a {playerOneRandomNum}.");
+
+                playerTwoRandomNum = randomNums.Next(1, 7);
+                Console.WriteLine($"{playerTwo} rolled a {playerTwoRandomNum}.");
+
+                if (playerOneRandomNum > playerTwoRandomNum)
+                {
+                    Console.WriteLine($"{playerOne} won!");
+                }
+                else if (playerOneRandomNum < playerTwoRandomNum)
+                {
+                    Console.WriteLine($"{playerTwo} won!");
+                }
+                else if (playerOneRandomNum == playerTwoRandomNum)
+                {
+                    Console.WriteLine("Tie!");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Dice-Game
+ Dice game (console application) programmed in C# (NET .Core 3.1) with single player and multiplayer option.
